54fe63d1a1859b5d0c546787e10c38dcc223ee32
asell22 <alexanderjsell@gmail.com>
2015-04-28 08:54:06 -0700

    Implemented signing in

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   end
 
   resources :users, except: [:destroy, :index]
+
+  get '/signin', to: "sessions#new"
+  post '/signin', to: "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
